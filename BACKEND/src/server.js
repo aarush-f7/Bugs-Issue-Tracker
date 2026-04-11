ffa4ee2428975ee198c1676819ee074c9d05fe42
Aarush Rajak <rajakaarush627@gmail.com>
2026-04-11 16:32:39 +0530

    final nail in the coffin

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -32,9 +32,22 @@ const app = express();
 // Global Middlewares
 // These run on EVERY incoming request
 // ─────────────────────────────────────────────
-
+// CORS configuration dynamic added:--------------------------------
+// ------------------------------------------------------------------
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+  // origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+  origin: function (origin, callback) {
+    const allowed = [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      process.env.FRONTEND_URL,
+    ].filter(Boolean);
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
